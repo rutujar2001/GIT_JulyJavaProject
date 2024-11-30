56dd362f69eb843051c202a4c3bdb522c5b0c885
Rutuja Rathod <rutujarathod50@gmail.com>
2024-11-30 16:23:17 +0530

    arraylist city code added

---
+++ JulyBatchJavaExample/src/com/velocity/JulyTest.java
@@ -0,0 +1,14 @@
+package com.velocity;
+
+import java.util.ArrayList;
+
+public class JulyTest {
+
+	public static void main(String[] args) {
+		ArrayList al = new ArrayList();
+		al.add("pune");
+		al.add("mumbai");
+		al.add("bangalore");
+		System.out.println(al);
+	}
+}
