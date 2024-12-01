989b04968ca37333b519966c19c5cee970ab0450
Rutuja Rathod <rutujarathod50@gmail.com>
2024-12-01 21:41:15 +0530

    added addition number code

--- JulyBatchJavaExample/src/com/velocity/TestSample.java
+++ JulyBatchJavaExample/src/com/velocity/TestSample.java
@@ -5,5 +5,9 @@ public class TestSample {
 	public static void main(String[] args) {
 
 		System.out.println("This is test sample file>>>");
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println("Addition is >> " + c);
 	}
 }
